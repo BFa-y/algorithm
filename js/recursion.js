34fc47e15544696432758c38093d3ccb56977d6c
BFa-y <18893715614@163.com>
2020-10-26 23:39:08 +0800

    递归

---
+++ js/recursion.js
@@ -0,0 +1,8 @@
+hanoi(disc, src, aux, dst){
+    if (disc > 0) {
+      hanoi(disc - 1, src, dst, aux);
+      console.log(' 移动 ' + disc + ' 号圆盘 ' + ' 从 ' + src + ' 移动到 ' + dst);
+      hanoi(disc - 1, aux, src, dst)
+    }
+  }
+hanoi(4, 'A', 'B', 'C')
