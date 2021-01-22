0fc45902c817432073e9553aca3d395f6a2a6a72
BFa-y <18893715614@163.com>
2021-01-22 08:56:38 +0800

    筛卡好好

--- vue/getTime.js
+++ vue/getTime.js
@@ -6,11 +6,16 @@ let list = []
 let acctList = [
     { ACCT: "1111111111", ACCT_TYPE: "1" },
     { ACCT: "2222222222", ACCT_TYPE: "3" },
+    { ACCT: "333333333", ACCT_TYPE: "4" },
     { ACCT: "333333333", ACCT_TYPE: "5" }
 ]
 for (var item of acctList) {
-    if (!(item.ACCT_TYPE === "3")) {
+    if (item.ACCT_TYPE != "3" && item.ACCT_TYPE != "4") {
         list.push(item)
     }
 }
 console.log(list)
+/*
+[ { ACCT: '1111111111', ACCT_TYPE: '1' },
+  { ACCT: '333333333', ACCT_TYPE: '5' } ]
+*/
