26b9f612c27f11ee091176a56c5be7de5928e306
BFa-y <18893715614@163.com>
2021-01-25 11:36:37 +0800

    v-on

--- vue/getTime.js
+++ vue/getTime.js
@@ -1,21 +1,7 @@
 // getTime()时间戳处理时间
-// let sta = new Date(20200101).getTime()
-
-// console.log(sta)
-let list = []
-let acctList = [
-    { ACCT: "1111111111", ACCT_TYPE: "1" },
-    { ACCT: "2222222222", ACCT_TYPE: "3" },
-    { ACCT: "333333333", ACCT_TYPE: "4" },
-    { ACCT: "333333333", ACCT_TYPE: "5" }
-]
-for (var item of acctList) {
-    if (item.ACCT_TYPE != "3" && item.ACCT_TYPE != "4") {
-        list.push(item)
-    }
-}
-console.log(list)
-/*
-[ { ACCT: '1111111111', ACCT_TYPE: '1' },
-  { ACCT: '333333333', ACCT_TYPE: '5' } ]
-*/
+let sta = new Date("2021/01/22").getTime()
+let sta1 = new Date().getTime()  //number
+// 30天的毫秒数
+let sec = 60*60*24*1000*3000
+// let con = 
+console.log(sta-sec)
