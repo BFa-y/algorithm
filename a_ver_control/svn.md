1c6f9c7335d5faa03936988c802e466b3e72c17e
BFa-y <18893715614@163.com>
2021-03-24 17:14:02 +0800

    date

---
+++ a_ver_control/svn.md
@@ -0,0 +1,6 @@
+Subversion-svn
+##### 基本概念：
+repository（源代码库）:源代码统一存放的地方
+Checkout（提取）: repository checkout代码
+Commit（提交）:修改代码，Commit到repository
+Update (更新): Update和Repository源代码同步
--- js/Date.js
+++ js/Date.js
@@ -18,20 +18,20 @@ var bb = "5"
 
 // 获取当前日期
 // var dateStart = date.getFullYear() + '-' + (date.getMonth() - 3) + '-' + date.getDate()
-// var year = "" + date.getFullYear()
-// var month = "" + (date.getMonth() +1)
-// var todays = "" + date.getDate()
-// if (month.length == 1) {
-//     month = "0" + month
-// }
-// if (todays.length == 1) {
-//     todays = "0" + todays
-// }
-// var dateStart = year + month + todays
+var year = "" + date.getFullYear()
+var month = "" + (date.getMonth() +1)
+var todays = "" + date.getDate()
+if (month.length == 1) {
+    month = "0" + month
+}
+if (todays.length == 1) {
+    todays = "0" + todays
+}
+var dateStart = year + month + todays
+console.log(dateStart)  //20200101 -八位时间格式
 // console.log(typeof(todays))
 // console.log(month)
 // console.log(todays)
-// console.log(dateStart)
 
 var aaa ="5266**** ****5856|4546546tr46y4t6j46t4j"
 var bbb =aaa.indexOf("(|))
